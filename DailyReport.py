861566a74108868a2062f5f1f0f45b7a0b9f43e3
之雨商 <1299172402@qq.com>
2021-02-24 13:36:33 +0800

    Update DailyReport.py

--- DailyReport.py
+++ DailyReport.py
@@ -1,7 +1,10 @@
 import requests
 
 def main():
-    print("Hello, Github Action!")
+    url = "http://tinywebdb.appinventor.space/api?user=dache&secret=a46a5f05&action=update&tag=123&value=748"
+    req = requests.post(url)
+    print(req)
+    print(req.text)
 
 if __name__ == '__main__':
     main()
