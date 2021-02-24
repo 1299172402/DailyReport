81617b7fcefba277f3ef82ffc2c4a153076c40e3
之雨商 <1299172402@qq.com>
2021-02-24 21:20:45 +0800

    Update TokenExpiredTest.py

--- TokenExpiredTest.py
+++ TokenExpiredTest.py
@@ -11,9 +11,12 @@ def main():
     res = requests.get(url, headers=headers)
     print(res.text)
 
-    localtime = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
-    url = f"http://sc.ftqq.com/SCU138613Tf980299c10ed0bc98b9620d68af8f0635fe415a3271b3.send?text=Token有效期监控 {localtime}&desp={res.text}"
-    requests.post(url)
+    normal_res = {"code":200,"msg":"ok","data":{"user_id":66443,"role":"student","truename":"胡洪韬","student_id":"2020011610","college_id":8,"college":"信息科学与工程学院","department":"","instructor_id":121,"instructor":"齐待弟","tutor":"","education":"本科","birthplace":"浙江","mobile":"无","create_time":"2020-09-07T10:59:28+08:00"}}
+    
+    if res.text != normal_res:
+        localtime = str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+        url = f"http://sc.ftqq.com/SCU138613Tf980299c10ed0bc98b9620d68af8f0635fe415a3271b3.send?text=Token有效期监控 {localtime}&desp={res.text}"
+        requests.post(url)
 
 if __name__ == "__main__":
     main()
