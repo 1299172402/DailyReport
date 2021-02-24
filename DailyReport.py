c1aa2ceb5f1f75dcc7073c2f55c4307fd910c199
之雨商 <1299172402@qq.com>
2021-02-24 13:50:59 +0800

    Update DailyReport.py

--- DailyReport.py
+++ DailyReport.py
@@ -1,10 +1,59 @@
 import requests
 
 def main():
-    url = "http://tinywebdb.appinventor.space/api?user=dache&secret=a46a5f05&action=update&tag=123&value=748"
-    req = requests.post(url)
-    print(req)
-    print(req.text)
+    url = "https://reserve.25team.com/wxappv1/yi/addReport"
+
+    headers  = {
+        'Host':'reserve.25.team',
+        'Connection':'keep-alive',
+        'Content-Length':'659',
+        'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat',
+        'content-type':'application/json',
+        'token':'********',  #需要你自己抓取来使用自己的token
+        'referer':'https://servicewechat.com/wxd2bebfc67ee4a7eb/58/page-frame.html',
+        'Accept-Encoding':'gzip,deflate,br'
+    }
+
+    data = {
+        "content": {
+            "0": "否",
+            "1": "",
+            "2": "",
+            "3": "",
+            "4": "",
+            "5": "否",
+            "6": "否",
+            "7": "否",
+            "8": "正常",
+            "9": "37.2及以下",
+            "10": "xx省xx市xx县 经纬度:123456789,987654321",
+            "11": "否",
+            "12": "",
+            "13": "",
+            "14": "正常（体温37.2及以下）",
+            "15": "否",
+            "16": ""
+        },
+        "version": 14,
+        "stat_content": {
+            "今日是否在京": "否",
+            "今日是否在湖北？": "否",
+            "今日是否“密切接触”疑似或确诊人群？": "否",
+            "今日是否在集中隔离点隔离": "否"
+        },
+        "location": {
+            "province": "湖南省",
+            "country": "中国",
+            "city": "邵阳市",
+            "longitude": 123456789,
+            "latitude": 987654321
+        },
+        "sick": "正常（体温37.2及以下）",
+        "accept_templateid": ""
+    }
+
+    res = requests.post(url, headers=headers, json=data)
+    print(res.text)
 
 if __name__ == '__main__':
     main()
