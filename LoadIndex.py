b723ca6ea11d61fa0d4aac2b2fda9916a9c19807
之雨商 <1299172402@qq.com>
2021-03-04 12:14:55 +0800

    Update LoadIndex.py

--- LoadIndex.py
+++ LoadIndex.py
@@ -7,7 +7,6 @@ def main():
     url = "https://reserve.25team.com/wxappv1/yi/index?version=16"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
-        "token": os.environ["mytoken"], # "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjQ0MywiZXhwIjoxNzExMzYwNzMwLCJpc3MiOiJnaW4tYmxvZyJ9.hD_uLNUVO0TJNczcwqxb3kBHXk5ywG5MKx4CIuxMv4A",
         "Referer": "https://servicewechat.com/wxd2bebfc67ee4a7eb/73/page-frame.html",
     }
     res_origin = requests.get(url, headers=headers).text
