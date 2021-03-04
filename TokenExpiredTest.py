58ddb16e04ad7c2bfff62c6582051140202924d2
之雨商 <1299172402@qq.com>
2021-03-04 12:15:31 +0800

    Update TokenExpiredTest.py

--- TokenExpiredTest.py
+++ TokenExpiredTest.py
@@ -6,7 +6,7 @@ def main():
     url = "https://reserve.25team.com/wxappv1/yi/getUpdateInfo"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
-        "token": os.environ["mytoken"], #"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo2NjQ0MywiZXhwIjoxNzExMzYwNzMwLCJpc3MiOiJnaW4tYmxvZyJ9.hD_uLNUVO0TJNczcwqxb3kBHXk5ywG5MKx4CIuxMv4A",
+        "token": os.environ["mytoken"],
         "Referer": "https://servicewechat.com/wxd2bebfc67ee4a7eb/73/page-frame.html",
     }
     res = requests.get(url, headers=headers)
